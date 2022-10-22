6e077c48005b375bccc5d7b765c4bbab91fc08e0
Vishal Sharma <vishalsharma34258@gmail.com>
2022-10-22 20:43:26 +0530

    first project completed

---
+++ file1/app.js
@@ -0,0 +1,17 @@
+const buttons = document.querySelectorAll('[data-carousel-button]');
+
+buttons.forEach(button => {
+    button.addEventListener('click',()=>
+    {
+        const offset = button.dataset.carouselButton === 'next'?1:-1;
+        const slides = button.closest('[data-carousel]').querySelector('[data-slides]');
+
+        const activeslide = slides.querySelector('[data-active]');
+        let newIndex = [...slides.children].indexOf(activeslide) + offset;
+        if(newIndex<0) newIndex = slides.children.length-1;
+        if(newIndex>=slides.children.length)newIndex = 0;
+
+        slides.children[newIndex].dataset.active = true;
+        delete activeslide.dataset.active;
+    })
+});
